468b1bab1049022376a11b693c33c0caf9661ec9
Kamil <46963243+wawrkam@users.noreply.github.com>
2026-05-06 21:01:28 +0200

    Add files via upload

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,5 +1,6 @@
 package hooks;
 
+import com.codeborne.selenide.Configuration;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -12,6 +13,9 @@ public class Hooks {
 
     @Before
     public void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10_000;
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
         open("about:blank");
     }
 
--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -1,6 +1,5 @@
 package runner;
 
-import base.BaseTest;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -17,8 +16,8 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-report.html",
                 "json:target/cucumber.json"
         },
-        tags = "@smoke",
+        tags = System.getProperty("tags", ""),
         monochrome = true
 )
-public class CucumberRunner extends BaseTest {
+public class CucumberRunner {
 }
